51377e2a06efde82eb8bfbf7ef63ed7ca52870a0
Lui <luiizhalexandretti@gmail.com>
2025-11-09 23:30:54 -0300

    Create publicEnums.js

---
+++ enums/publicEnums.js
@@ -0,0 +1,19 @@
+const BLOODGROUP = Object.freeze({
+    A_POSITIVE: 'A_POSITIVE',
+    A_NEGATIVE: 'A_NEGATIVE',
+    B_POSITIVE: 'B_POSITIVE',
+    B_NEGATIVE: 'B_NEGATIVE',
+    AB_POSITIVE: 'AB_POSITIVE',
+    AB_NEGATIVE: 'AB_NEGATIVE',
+    O_POSITIVE: 'O_POSITIVE',
+    O_NEGATIVE: 'O_NEGATIVE',
+    NOT_DEFINED_YET: 'NOT_DEFINED_YET',
+});
+
+const ROLES = Object.freeze({
+    DONOR: 'DONOR',
+    ADMIN: 'ADMIN',
+    HOSPITAL: 'HOSPITAL'
+})
+
+module.exports = { BLOODGROUP, ROLES }
